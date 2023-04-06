2284fad752625c53fabf45ac07aaa2c06d915197
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2023-04-06 12:09:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+// Get all the price cells in the table
+const priceCells = document.querySelectorAll('td[data-ns-test="prices"]');
+
+// Calculate the total price by adding up all the prices
+let totalPrice = 0;
+priceCells.forEach(cell => {
+  totalPrice += parseFloat(cell.textContent);
+});
+
+// Add the total price to the grand total cell in the table
+const grandTotalCell = document.querySelector('td[data-ns-test="grandTotal"]');
+grandTotalCell.textContent = totalPrice.toFixed(2);
